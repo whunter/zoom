1064df73050d80c8066333c32b317162d3ac9cf4
Lee Hunter <wleehunter@gmail.com>
2012-07-27 09:24:48 -0400

    Initial commit

---
+++ js/plugins.js
@@ -0,0 +1,266 @@
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+
+// make it safe to use console.log always
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+
+
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+/**
+ * author Remy Sharp
+ * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ */
+(function ($) {
+    function getViewportHeight() {
+        var height = window.innerHeight; // Safari, Opera
+        var mode = document.compatMode;
+
+        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
+            height = (mode == 'CSS1Compat') ?
+            document.documentElement.clientHeight : // Standards
+            document.body.clientHeight; // Quirks
+        }
+
+        return height;
+    }
+
+    $(window).scroll(function () {
+        var vpH = getViewportHeight(),
+            scrolltop = (document.documentElement.scrollTop ?
+                document.documentElement.scrollTop :
+                document.body.scrollTop),
+            elems = [];
+        
+        // naughty, but this is how it knows which elements to check for
+        $.each($.cache, function () {
+            if (this.events && this.events.inview) {
+                elems.push(this.handle.elem);
+            }
+        });
+
+        if (elems.length) {
+            $(elems).each(function () {
+                var $el = $(this),
+                    top = $el.offset().top,
+                    height = $el.height(),
+                    inview = $el.data('inview') || false;
+
+                if (scrolltop > (top + height) || scrolltop + vpH < top) {
+                    if (inview) {
+                        $el.data('inview', false);
+                        $el.trigger('inview', [ false ]);                        
+                    }
+                } else if (scrolltop < (top + height)) {
+                    if (!inview) {
+                        $el.data('inview', true);
+                        $el.trigger('inview', [ true ]);
+                    }
+                }
+            });
+        }
+    });
+    
+    // kick the event to pick up any elements already in view.
+    // note however, this only works if the plugin is included after the elements are bound to 'inview'
+    $(function () {
+        $(window).scroll();
+    });
+})(jQuery);
+
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+/**
+ * jQuery.LocalScroll - Animated scrolling navigation, using anchors.
+ * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 3/11/2009
+ * @author Ariel Flesler
+ * @version 1.2.7
+ **/
+(function($){var l=location.href.replace(/#.*/,'');var g=$.localScroll=function(a){$('body').localScroll(a)};g.defaults={duration:1e3,axis:'y',event:'click',stop:true,target:window,reset:true};g.hash=function(a){if(location.hash){a=$.extend({},g.defaults,a);a.hash=false;if(a.reset){var e=a.duration;delete a.duration;$(a.target).scrollTo(0,a);a.duration=e}i(0,location,a)}};$.fn.localScroll=function(b){b=$.extend({},g.defaults,b);return b.lazy?this.bind(b.event,function(a){var e=$([a.target,a.target.parentNode]).filter(d)[0];if(e)i(a,e,b)}):this.find('a,area').filter(d).bind(b.event,function(a){i(a,this,b)}).end().end();function d(){return!!this.href&&!!this.hash&&this.href.replace(this.hash,'')==l&&(!b.filter||$(this).is(b.filter))}};function i(a,e,b){var d=e.hash.slice(1),f=document.getElementById(d)||document.getElementsByName(d)[0];if(!f)return;if(a)a.preventDefault();var h=$(b.target);if(b.lock&&h.is(':animated')||b.onBefore&&b.onBefore.call(b,a,f,h)===false)return;if(b.stop)h.stop(true);if(b.hash){var j=f.id==d?'id':'name',k=$('<a> </a>').attr(j,d).css({position:'absolute',top:$(window).scrollTop(),left:$(window).scrollLeft()});f[j]='';$('body').prepend(k);location=e.hash;k.remove();f[j]=d}h.scrollTo(f,b).trigger('notify.serialScroll',[f])}})(jQuery);
+
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+/**
+ * jQuery.ScrollTo - Easy element scrolling using jQuery.
+ * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 5/25/2009
+ * @author Ariel Flesler
+ * @version 1.4.2
+ *
+ * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
+ */
+(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||a.ownerDocument||a;return d.browser.safari||e.compatMode=='BackCompat'?e.body:e.documentElement})};d.fn.scrollTo=function(n,j,b){if(typeof j=='object'){b=j;j=0}if(typeof b=='function')b={onAfter:b};if(n=='max')n=9e9;b=d.extend({},k.defaults,b);j=j||b.speed||b.duration;b.queue=b.queue&&b.axis.length>1;if(b.queue)j/=2;b.offset=p(b.offset);b.over=p(b.over);return this._scrollable().each(function(){var q=this,r=d(q),f=n,s,g={},u=r.is('html,body');switch(typeof f){case'number':case'string':if(/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(f)){f=p(f);break}f=d(f,this);case'object':if(f.is||f.style)s=(f=d(f)).offset()}d.each(b.axis.split(''),function(a,i){var e=i=='x'?'Left':'Top',h=e.toLowerCase(),c='scroll'+e,l=q[c],m=k.max(q,i);if(s){g[c]=s[h]+(u?0:l-r.offset()[h]);if(b.margin){g[c]-=parseInt(f.css('margin'+e))||0;g[c]-=parseInt(f.css('border'+e+'Width'))||0}g[c]+=b.offset[h]||0;if(b.over[h])g[c]+=f[i=='x'?'width':'height']()*b.over[h]}else{var o=f[h];g[c]=o.slice&&o.slice(-1)=='%'?parseFloat(o)/100*m:o}if(/^\d+$/.test(g[c]))g[c]=g[c]<=0?0:Math.min(g[c],m);if(!a&&b.queue){if(l!=g[c])t(b.onAfterFirst);delete g[c]}});t(b.onAfter);function t(a){r.animate(g,j,b.easing,a&&function(){a.call(this,n,b)})}}).end()};k.max=function(a,i){var e=i=='x'?'Width':'Height',h='scroll'+e;if(!d(a).is('html,body'))return a[h]-d(a)[e.toLowerCase()]();var c='client'+e,l=a.ownerDocument.documentElement,m=a.ownerDocument.body;return Math.max(l[h],m[h])-Math.min(l[c],m[c])};function p(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------
+
+/*
+License: http://creativecommons.org/licenses/by-sa/3.0/ (Attribution Share Alike). Please attribute work to Ian Lunn simply by leaving these comments in the source code or if you'd prefer, place a link on your website to http://www.ianlunn.co.uk/.
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+*/
+
+$(document).ready(function() { //when the document is ready...
+
+	//save selectors as variables to increase performance
+	var $window = $(window);
+	var $roomOne = $('#room-one');
+	var $text = $('.text');
+	var $initText = new Object();
+	$initText.width = $text.width();
+	$initText.height = $text.height();
+	$initText.fontSize = parseInt($text.css('font-size'));
+	$initText.left = parseInt($text.css('margin-left'));
+	$initText.top = parseInt($text.css('margin-top'));
+	
+	$scaledText = $('.scaled-text');
+	$scaledText.css({'width': $window.width()});
+	$initScaled = new Object();
+	$initScaled.marginTop = parseInt($scaledText.css('margin-top'));
+
+	var windowHeight = $window.height(); //get the height of the window
+
+	$text.fadeIn(800);
+
+	//apply the class "inview" to a section that is in the viewport
+	$('#room-one').bind('inview', function (event, visible) {
+			if (visible == true) {
+			$(this).addClass("inview");
+			} else {
+			$(this).removeClass("inview");
+			}
+		});
+	
+			
+	//function that places the navigation in the center of the window
+	function RepositionNav(){
+		var windowHeight = $window.height(); //get the height of the window
+		var navHeight = $('#nav').height() / 2;
+		var windowCenter = (windowHeight / 2); 
+		var newtop = windowCenter - navHeight;
+		$('#nav').css({"top": newtop}); //set the new top position of the navigation list
+	}
+	
+	//function that is called for every pixel the user scrolls. Determines the position of the background
+	/*arguments: 
+		x = horizontal position of background
+		windowHeight = height of the viewport
+		pos = position of the scrollbar
+		adjuster = adjust the position of the background
+		inertia = how fast the background moves in relation to scrolling
+	*/
+	function newPos(x, windowHeight, pos, adjuster, inertia){
+		return x + "% " + (-((windowHeight + pos) - adjuster) * inertia)  + "px";
+	}
+
+	function getNewDivHeight(initWidth, initHeight, pos) {
+		var ratio = getWidthByHeightRatio(initWidth, initHeight);
+		var adjustedPos = pos / 10;
+		return initHeight + parseInt(pos);
+	}
+
+	function getNewDivWidth(initWidth, initHeight, pos) {
+		var ratio = getWidthByHeightRatio(initWidth, initHeight);
+		var adjustedPos = pos / 10;
+		return initWidth + parseInt(ratio * pos);
+	}
+
+	function getWidthByHeightRatio(width, height) {
+		if(!isNaN(width) && !isNaN(height)) {
+			return parseFloat(width) / parseFloat(height);
+		}
+		return false;
+	}
+	
+	//function to be called whenever the window is scrolled or resized
+	function Move(){ 
+		var pos = $window.scrollTop(); //position of the scrollbar
+
+		//if the first section is in view...
+		if($roomOne.hasClass("inview")){
+			//call the newPos function and change the background position
+			
+			var dx = getNewDivWidth(1080, 720,pos) - 1080;
+			var xpos = dx / -2;
+			var dy = getNewDivHeight(1080, 720, pos) - 720;
+			var ypos = (dy / -2);
+			$roomOne.animate({
+				marginLeft: xpos + 'px',
+				marginTop: ypos + 'px',
+				width: getNewDivWidth(1080,720,pos),
+				height: getNewDivHeight(1080,720,pos)
+			}, 10, function(){
+				// response
+			});
+			
+		//if(parseInt($text.css('width')) > $window.width() - 50) {
+			var textDx = getNewDivWidth($window.width() * 4, $window.width(), pos) - $window.width();
+			var textXpos = (textDx / - 2);
+			$scaledText.animate({
+				marginTop: $initScaled.marginTop - (pos / 5),
+				marginLeft: textXpos + 'px',
+				width: getNewDivWidth($window.width() * 4, $window.width(), pos)
+			}, 10, function(){
+				//console.log($scaledText.css('margin-top'));
+			});
+		//}
+
+			$text.animate({
+				fontSize: $initText.fontSize + (pos / 2)
+			}, 10, function(){
+				//console.log(pos + ' ' + $text.css('margin-top'));
+			});
+
+			if(parseInt($text.css('font-size')) > 1000)
+			{
+				$text.hide();
+			}
+			else {
+				$text.show();
+			}
+
+				
+		}
+
+
+		
+	}
+		
+	RepositionNav(); //Reposition the Navigation to center it in the window when the script loads
+	
+	$window.resize(function(){ //if the user resizes the window...
+		Move(); //move the background images in relation to the movement of the scrollbar
+		RepositionNav(); //reposition the navigation list so it remains vertically central
+	});		
+	
+	$window.bind('scroll', function(){ //when the user is scrolling...
+		Move(); //move the background images in relation to the movement of the scrollbar
+	});
+	
+	 $('#nav').localScroll();
+});
+
+
+
+
+
+ 
+
+
+
